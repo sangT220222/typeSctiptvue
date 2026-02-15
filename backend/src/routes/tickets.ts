3f5838ab1fad857d0ea55df313e8397fd5a82428
Sangam <100322380+sangT220222@users.noreply.github.com>
2026-02-15 15:28:06 +0000

    update/patch function implemented

--- backend/src/routes/tickets.ts
+++ backend/src/routes/tickets.ts
@@ -7,8 +7,9 @@ export const ticketsRouter = Router();
 
 ticketsRouter.get("/", async (req, res) => {
   const tickets = await prisma.ticket.findMany();
+  tickets.sort((a, b) => a.priority.localeCompare(b.priority));
+  console.log(tickets);
   res.json(tickets);
-  console.log("TICKETS FETCHED");
 });
 
 const TicketStatus = z.enum(["in_progress", "todo", "done"]);
@@ -22,10 +23,29 @@ const ticketSchema = z.object({
 
 ticketsRouter.post("/", async (req, res) => {
   try {
-    // const ticketData = ticketSchema.parse(req.body);
-    const ticketData = await prisma.ticket.create({ data: req.body });
+    const ticketData = await ticketSchema.parseAsync(req.body);
     return res.status(201).json(ticketData);
   } catch (error) {
     return res.status(400).json({ error: "Invalid" });
+  }
+});
+
+ticketsRouter.patch("/:id", async (req, res) => {
+  try {
+    const sentID = req.params.id;
+    const sentBody = req.body;
+    const matchedTicker = await prisma.ticket.findUnique({
+      where: { id: sentID },
+    });
+    if (matchedTicker && sentBody.status !== matchedTicker.status) {
+      //update status in the db
+      await prisma.ticket.update({
+        where: { id: sentID },
+        data: { status: sentBody.status },
+      });
+      return res.status(200).json({ message: "Status updated successfully" });
+    }
+  } catch (error) {
+    return res.status(400).json({ error: "Request invalid" });
   }
 });
