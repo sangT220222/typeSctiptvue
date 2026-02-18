0dece8021eae00f0998ee957b00ecfbd4afcc6bb
Sangam <100322380+sangT220222@users.noreply.github.com>
2026-02-18 22:49:22 +0000

    General updating of ticket been implemented - title & description can be updated

--- backend/src/routes/tickets.ts
+++ backend/src/routes/tickets.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { z } from "zod";
 import { prisma } from "../db.js";
 import { de } from "zod/locales";
+import { match } from "node:assert";
 export const ticketsRouter = Router();
 
 ticketsRouter.get("/", async (req, res) => {
@@ -66,7 +67,7 @@ ticketsRouter.patch("/:id/status", async (req, res) => {
   //if status is the same as db, return 200;
   if (matchedTicket.status === sentBody.status) {
     return res.status(200).json({
-      message: "Status is the same as current status, no update needed!",
+      message: "Status is the same as current status, no update needed",
     });
   }
 
@@ -81,12 +82,92 @@ ticketsRouter.patch("/:id/status", async (req, res) => {
 
   try {
     //update status in the db
-    await prisma.ticket.update({
+    const updatedTicket = await prisma.ticket.update({
       where: { id: sentID },
       data: { status: sentBody.status },
     });
-    return res.status(200).json({ message: "Status updated successfully" });
+    return res
+      .status(200)
+      .json({ message: "Status updated successfully", ticket: updatedTicket });
   } catch (error) {
     return res.status(500).json({ error: "Internal server error" });
   }
+});
+
+//general update for ticket
+ticketsRouter.patch("/:id", async (req, res) => {
+  //users can update title, description, status and priority
+  //get request params body
+  if (!req.params.id) {
+    return res
+      .status(400)
+      .json({ error: "ID not provided, please provide it" });
+  }
+  //see if id is in the database
+  const sentID = req.params.id;
+  const matchedTicket = await prisma.ticket.findUnique({
+    where: {
+      id: sentID,
+    },
+  });
+  if (!matchedTicket) {
+    return res.status(404).json({
+      error: "ID provided doesn't match our system, please input a correct one",
+    });
+  }
+
+  //now we can do validation of body
+  //we want users to be able to update 3 fields.
+  const updateTitleValue = req.body.title;
+  const updateDescriptionValue = req.body.description;
+  const updateStatusValue = req.body.status;
+  const updatePriorityValue = req.body.priority;
+
+  if (
+    !updateTitleValue &&
+    !updateDescriptionValue &&
+    !updateStatusValue &&
+    !updatePriorityValue
+  ) {
+    return res.status(400).json({
+      error:
+        "Please provide valid body - include title, description, status or priorit ",
+    });
+  }
+
+  //check if value is the same as existing
+  if (
+    matchedTicket.title === req.body.title ||
+    matchedTicket.description === req.body.description
+  ) {
+    return res
+      .status(400)
+      .json({ error: "Value is the same, please send a different one" });
+  }
+
+  //check what is in the request body - can be either title only, description only, status only, priority only, or multiple of them
+  let updatedTicket = null;
+  if (updateTitleValue) {
+    updatedTicket = await prisma.ticket.update({
+      where: { id: sentID },
+      data: { title: req.body.title },
+    });
+  }
+  if (updateDescriptionValue) {
+    updatedTicket = await prisma.ticket.update({
+      where: { id: sentID },
+      data: { description: req.body.description },
+    });
+  }
+  // if(updateStatusValue)
+  // {
+
+  // }
+  // if(updatePriorityValue)
+  // {
+
+  // }
+  return res
+    .status(200)
+    .json({ message: "Ticket been updated", updated_ticket: updatedTicket });
 });
